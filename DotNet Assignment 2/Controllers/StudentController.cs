ee954f01e7a67790a591cc430049c601b8a3796e
nhrepon <nurhossainrepon7248@gmail.com>
2024-10-18 22:40:03 +0600

    version: 1.0.0

---
+++ DotNet Assignment 2/Controllers/StudentController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DotNet_Assignment_2.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult StudentList()
+        {
+            return View();
+        }
+    }
+}
